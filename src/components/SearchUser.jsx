6aa7363d718b33c8c28e0022437c8cf44972ed3a
fatih <fthhbsr@gmail.com>
2022-07-06 17:19:14 +0300

    home added

--- src/components/SearchUser.jsx
+++ src/components/SearchUser.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
-const SearchUser = () => {
+const SearchUser = ({ handleChange }) => {
   return (
-    <div>SearchUser</div>
-  )
-}
+    <div className="row">
+      <div className="col-md-4 mx-auto">
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="search">Search</InputGroup.Text>
+          <Form.Control
+            type="search"
+            onChange={handleChange}
+            aria-describedby="search"
+          />
+        </InputGroup>
+      </div>
+    </div>
+  );
+};
 
-export default SearchUser
+export default SearchUser;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import SearchUser from "../components/SearchUser";
+import Followers from "../components/Followers";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [allFollowers, setAllFollowers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+
+  const getFollowers = async () => {
+    const { data } = await axios.get(
+      "https://api.github.com/users/fthhbsr/followers?per_page=100"
+    );
+    console.log(data);
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    getFollowers();
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  const followersList = allFollowers.filter((follower) =>
+    follower.login.includes(search)
+  );
+
+  return (
+    <div>
+      <SearchUser handleChange={handleChange} />
+      <Followers followers={(loading, followersList)} />
+    </div>
+  );
 };
 
 export default Home;
